fc79ee7574296b7413bf1d9fb58e8361721191bf
MostafaGamal <motafa.gamal@codzone-eg.com>
2023-02-05 17:43:24 +0200

    pages added

---
+++ OrchardHeadlessCMS/Pages/BlogPost.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrchardHeadlessCMS.Pages
+{
+    public class BlogPostsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ OrchardHeadlessCMS/Pages/BlogPosts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrchardHeadlessCMS.Pages
+{
+    public class BlogPostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
